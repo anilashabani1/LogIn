e7e72b052fb214c6a4160ae7f47c8f3a8e40077c
anilashabani1 <anilashabani8@gmail.com>
2024-07-11 11:51:09 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,8 @@
+while True:
+    emri = input("sheno username:")
+    if emri == "anilashabani":
+        continue
+    password = input("Shkruaje passwordin:")
+    if password == "jcoders":
+         break
+print("Emri dhe Password jane ne rregull")
